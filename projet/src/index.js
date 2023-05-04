031b71a3e893f541987a88a2dee32e39b993b4db
Marion de Kerchove <marion.dekerchove@heig-vd.ch>
2023-05-04 09:11:40 +0200

    commit6

--- projet/src/index.js
+++ projet/src/index.js
@@ -98,42 +98,13 @@ function getDropOffLocationsByHour(data, hour) {
 
 //console.log(getDropOffLocationsByHour(data1405, 0));
 
-//max : lat: 40.953047, long: -74.071165 -> au-dessus de Paramus
-//min: lat :40.588219, long: -73.657818 -> près de long beach
-
-/* Heatmap */
-
-/* const heatMapArray = [];
-
-const heatMap = L.heatLayer(heatMapArray, {
-  radius: 40,
-  blur: 13,
-  minOpacity: 0.15,
-  gradient: { 0.1: "#FF33B1", 0.5: "#16F2F2", 0.7: "#A0FF00" },
-});
-
-function renderHeatMapLayer(map, cleanData) {
-  cleanData.forEach((d) => {
-    let heatMapPoint = {
-      lat: d.pickup_latitude,
-      lon: d.pickup_longitude,
-    };
-
-    heatMapArray.push(heatMapPoint);
-  });
-
-  heatMap.addTo(map);
-} */
-
-//renderHeatMapLayer(map, cleanData);
-
 /* Heatmap par heure */
 
-const heatMapArray2 = [];
+const heatMapArray = [];
 
-const heatMap2 = L.heatLayer(heatMapArray2, {
+const heatMap = L.heatLayer(heatMapArray, {
   backgroundColor: "rgba(0,0,0,.95)",
-  radius: 10,
+  radius: 15,
   blur: 13,
   minOpacity: 0.3,
   maxOpacity: 0.9,
@@ -147,16 +118,58 @@ function renderHeatMapLayerByHour(map, cleanData, hour) {
         lon: d.pickup_longitude,
       };
 
-      heatMapArray2.push(heatMapPoint);
+      heatMapArray.push(heatMapPoint);
     }
   });
 
-  heatMap2.addTo(map);
+  heatMap.addTo(map);
+
+  console.log(heatMapArray);
+}
+
+//renderHeatMapLayerByHour(map, cleanData, 0);
+
+/*2ème map*/
+
+const map2 = L.map("map2").setView([40.764477, -73.979113], 10);
+L.tileLayer(
+  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+  {
+    maxZoom: 20,
+    attribution:
+      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+  }
+).addTo(map2);
+
+const heatMapArray2 = [];
+
+const heatMap2 = L.heatLayer(heatMapArray2, {
+  backgroundColor: "rgba(0,0,0,.95)",
+  radius: 15,
+  blur: 13,
+  minOpacity: 0.3,
+  maxOpacity: 0.9,
+  //gradient: { 0.1: "#A0FF00", 0.5: "#16F2F2", 0.7: "#ff33b1" },
+});
+
+function renderHeatMapLayerByHour2(map2, cleanData, hour) {
+  cleanData.forEach((d) => {
+    if (d.dropoff_hour === hour) {
+      let heatMapPoint2 = {
+        lat: d.dropoff_latitude,
+        lon: d.dropoff_longitude,
+      };
+
+      heatMapArray2.push(heatMapPoint2);
+    }
+  });
+
+  heatMap2.addTo(map2);
 
   console.log(heatMapArray2);
 }
 
-renderHeatMapLayerByHour(map, cleanData, 0);
+renderHeatMapLayerByHour2(map2, cleanData, 0);
 
 /*Animation slider*/
 
@@ -165,13 +178,13 @@ const margin = { top: 50, right: 40, bottom: 50, left: 40 },
   width = window.innerWidth - margin.left - margin.right,
   height = 0.6 * window.innerHeight - margin.top - margin.bottom;
 
-const svg = d3
+/* const svg = d3
   .select("#viz_area")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   // translate this svg element to leave some margin.
-  .append("g");
+  .append("g"); */
 
 // variable to store our intervalID
 let nIntervId;
@@ -185,14 +198,13 @@ function animate() {
 
 let i = 0;
 function play() {
-  if (i == heatMapArray2.length - 1) {
+  if (i > 24) {
     i = 0;
   } else {
     i++;
+    //d3.select("#paragraphe").text(data[i].annee);
+    renderHeatMapLayerByHour(map, cleanData, i);
   }
-
-  d3.select("#paragraphe").text(data[i].annee);
-  getPickupLocationsByHour(map, cleanData, i);
 }
 
 function stop() {
@@ -214,45 +226,3 @@ function pause() {
 document.getElementById("play").addEventListener("click", animate);
 document.getElementById("stop").addEventListener("click", stop);
 document.getElementById("pause").addEventListener("click", pause);
-
-/*2ème map*/
-
-const map2 = L.map("map2").setView([40.764477, -73.979113], 10);
-L.tileLayer(
-  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-  {
-    maxZoom: 20,
-    attribution:
-      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-  }
-).addTo(map2);
-
-const heatMapArray3 = [];
-
-const heatMap3 = L.heatLayer(heatMapArray3, {
-  backgroundColor: "rgba(0,0,0,.95)",
-  radius: 10,
-  blur: 13,
-  minOpacity: 0.3,
-  maxOpacity: 0.9,
-  //gradient: { 0.1: "#A0FF00", 0.5: "#16F2F2", 0.7: "#ff33b1" },
-});
-
-function renderHeatMapLayerByHour2(map2, cleanData, hour) {
-  cleanData.forEach((d) => {
-    if (d.dropoff_hour === hour) {
-      let heatMapPoint2 = {
-        lat: d.dropoff_latitude,
-        lon: d.dropoff_longitude,
-      };
-
-      heatMapArray3.push(heatMapPoint2);
-    }
-  });
-
-  heatMap3.addTo(map2);
-
-  console.log(heatMapArray3);
-}
-
-renderHeatMapLayerByHour2(map2, cleanData, 1);
