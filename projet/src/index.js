5dac9c5213697f66bc8c294e924b2dff4a4bff3e
Marion de Kerchove <marion.dekerchove@heig-vd.ch>
2023-05-04 08:45:58 +0200

    commit5

--- projet/src/index.js
+++ projet/src/index.js
@@ -13,38 +13,8 @@ import h337 from "heatmap.js";
 import * as L from "leaflet";
 import "leaflet.heat";
 
-/* import data1erMai from "../data/trip_data_0105.csv";
-import data0105 from "../data/trip_data_20130501.csv";
-import data0205 from "../data/trip_data_20130502.csv";
-import data0305 from "../data/trip_data_20130503.csv";
-import data0405 from "../data/trip_data_20130504.csv";
-import data0505 from "../data/trip_data_20130505.csv";
-import data0605 from "../data/trip_data_20130506.csv";
-import data0705 from "../data/trip_data_20130507.csv";
-import data0805 from "../data/trip_data_20130508.csv";
-import data0905 from "../data/trip_data_20130509.csv";
-import data1005 from "../data/trip_data_20130510.csv";
-import data1105 from "../data/trip_data_20130511.csv";
-import data1205 from "../data/trip_data_20130512.csv";
-import data1305 from "../data/trip_data_20130513.csv"; */
 import data1405 from "../data/trip_data_20130514.csv";
-/* import data1505 from "../data/trip_data_20130515.csv";
-import data1605 from "../data/trip_data_20130516.csv";
-import data1705 from "../data/trip_data_20130517.csv";
-import data1805 from "../data/trip_data_20130518.csv";
-import data1905 from "../data/trip_data_20130519.csv";
-import data2005 from "../data/trip_data_20130520.csv";
-import data2105 from "../data/trip_data_20130521.csv";
-import data2205 from "../data/trip_data_20130522.csv";
-import data2305 from "../data/trip_data_20130523.csv";
-import data2405 from "../data/trip_data_20130524.csv";
-import data2505 from "../data/trip_data_20130525.csv";
-import data2605 from "../data/trip_data_20130526.csv";
-import data2705 from "../data/trip_data_20130527.csv";
-import data2805 from "../data/trip_data_20130528.csv";
-import data2905 from "../data/trip_data_20130529.csv";
-import data3005 from "../data/trip_data_20130530.csv";
-import data3105 from "../data/trip_data_20130531.csv"; */
+/* import data1505 from "../data/trip_data_20130515.csv"; */
 
 /*Carte*/
 
@@ -75,11 +45,11 @@ console.log(cleanData);
 
 /* Slider */
 
-const div = document.querySelector("#slider");
+const divSlider = document.querySelector("#slider");
 const slider = sliderBottom().min(0).max(24).step(1).width(1000);
 
 const g = d3
-  .select(div)
+  .select(divSlider)
   .append("svg")
   .attr("width", 1200)
   .attr("height", 100)
@@ -133,9 +103,16 @@ function getDropOffLocationsByHour(data, hour) {
 
 /* Heatmap */
 
-function renderHeatMapLayer(map, cleanData) {
-  const heatMapArray = [];
+/* const heatMapArray = [];
 
+const heatMap = L.heatLayer(heatMapArray, {
+  radius: 40,
+  blur: 13,
+  minOpacity: 0.15,
+  gradient: { 0.1: "#FF33B1", 0.5: "#16F2F2", 0.7: "#A0FF00" },
+});
+
+function renderHeatMapLayer(map, cleanData) {
   cleanData.forEach((d) => {
     let heatMapPoint = {
       lat: d.pickup_latitude,
@@ -145,12 +122,137 @@ function renderHeatMapLayer(map, cleanData) {
     heatMapArray.push(heatMapPoint);
   });
 
-  const heatMap = L.heatLayer(heatMapArray, {
-    radius: 40,
-    blur: 13,
-    minOpacity: 0.15,
-    gradient: { 0.1: "#FF33B1", 0.5: "#16F2F2", 0.7: "#A0FF00" },
-  }).addTo(map);
+  heatMap.addTo(map);
+} */
+
+//renderHeatMapLayer(map, cleanData);
+
+/* Heatmap par heure */
+
+const heatMapArray2 = [];
+
+const heatMap2 = L.heatLayer(heatMapArray2, {
+  backgroundColor: "rgba(0,0,0,.95)",
+  radius: 10,
+  blur: 13,
+  minOpacity: 0.3,
+  maxOpacity: 0.9,
+});
+
+function renderHeatMapLayerByHour(map, cleanData, hour) {
+  cleanData.forEach((d) => {
+    if (d.pickup_hour === hour) {
+      let heatMapPoint = {
+        lat: d.pickup_latitude,
+        lon: d.pickup_longitude,
+      };
+
+      heatMapArray2.push(heatMapPoint);
+    }
+  });
+
+  heatMap2.addTo(map);
+
+  console.log(heatMapArray2);
 }
 
-renderHeatMapLayer(map, cleanData);
+renderHeatMapLayerByHour(map, cleanData, 0);
+
+/*Animation slider*/
+
+/*Animation heatmap*/
+const margin = { top: 50, right: 40, bottom: 50, left: 40 },
+  width = window.innerWidth - margin.left - margin.right,
+  height = 0.6 * window.innerHeight - margin.top - margin.bottom;
+
+const svg = d3
+  .select("#viz_area")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  // translate this svg element to leave some margin.
+  .append("g");
+
+// variable to store our intervalID
+let nIntervId;
+
+function animate() {
+  // check if already an interval has been set up
+  if (!nIntervId) {
+    nIntervId = setInterval(play, 1000);
+  }
+}
+
+let i = 0;
+function play() {
+  if (i == heatMapArray2.length - 1) {
+    i = 0;
+  } else {
+    i++;
+  }
+
+  d3.select("#paragraphe").text(data[i].annee);
+  getPickupLocationsByHour(map, cleanData, i);
+}
+
+function stop() {
+  clearInterval(nIntervId);
+
+  // release our intervalID from the variable
+  nIntervId = null;
+
+  i = 0;
+}
+
+function pause() {
+  clearInterval(nIntervId);
+
+  // release our intervalID from the variable
+  nIntervId = null;
+}
+
+document.getElementById("play").addEventListener("click", animate);
+document.getElementById("stop").addEventListener("click", stop);
+document.getElementById("pause").addEventListener("click", pause);
+
+/*2ème map*/
+
+const map2 = L.map("map2").setView([40.764477, -73.979113], 10);
+L.tileLayer(
+  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+  {
+    maxZoom: 20,
+    attribution:
+      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+  }
+).addTo(map2);
+
+const heatMapArray3 = [];
+
+const heatMap3 = L.heatLayer(heatMapArray3, {
+  backgroundColor: "rgba(0,0,0,.95)",
+  radius: 10,
+  blur: 13,
+  minOpacity: 0.3,
+  maxOpacity: 0.9,
+  //gradient: { 0.1: "#A0FF00", 0.5: "#16F2F2", 0.7: "#ff33b1" },
+});
+
+function renderHeatMapLayerByHour2(map2, cleanData, hour) {
+  cleanData.forEach((d) => {
+    if (d.dropoff_hour === hour) {
+      let heatMapPoint2 = {
+        lat: d.dropoff_latitude,
+        lon: d.dropoff_longitude,
+      };
+
+      heatMapArray3.push(heatMapPoint2);
+    }
+  });
+
+  heatMap3.addTo(map2);
+
+  console.log(heatMapArray3);
+}
+
+renderHeatMapLayerByHour2(map2, cleanData, 1);
